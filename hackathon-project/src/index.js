d873d5c65796fdcab57780c2c6b9a5bdcbe18d73
Katherine Chao <katherinechao@Katherines-MacBook-Pro.local>
2023-02-04 18:37:45 -0600

    mail

--- hackathon-project/src/index.js
+++ hackathon-project/src/index.js
@@ -7,9 +7,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1> The Gender Gap</h1>
-        //<a class="btn btn-warning" href="" id="email" target = "_" > click here</a>
+        <h1 className="siteName"> WGAP </h1>
         <button onClick={() => window.location = 'mailto:kat2chao@gmail.com'}>Contact Me</button>
+       
       </div>
 
     

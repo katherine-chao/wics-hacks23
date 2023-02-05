7ca34b9391449901300447668ef739e949230a73
Katherine Chao <katherinechao@Katherines-MacBook-Pro.local>
2023-02-04 18:21:20 -0600

    email opens

--- hackathon-project/src/index.js
+++ hackathon-project/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> The Gender Gap</h1>
+        //<a class="btn btn-warning" href="" id="email" target = "_" > click here</a>
+        <button onClick={() => window.location = 'mailto:kat2chao@gmail.com'}>Contact Me</button>
+      </div>
+
+    
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
